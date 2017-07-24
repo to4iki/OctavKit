d91a5845442a8353ee4b34999d722c3a97d5f333
tsk takezawa <tsk.take815@gmail.com>
2017-07-24 20:37:37 +0900

    Update Conference model

--- OctavKit/model/Conference.swift
+++ OctavKit/model/Conference.swift
@@ -2,6 +2,9 @@ import Himotoki
 
 public struct Conference {
     public let id: Id<Conference>
+    public let title: String
+    public let about: String
+    public let contact: String
     public let schedules: [Schedule]
     public let staffs: [Staff]
     public let venues: [Venue]
@@ -13,6 +16,9 @@ extension Conference: Encodable {
     public func encodeJSON() -> [String : Any] {
         return [
             "id": id.value,
+            "title": title,
+            "description": about,
+            "contact_information": contact,
             "dates": schedules.map({ $0.encodeJSON() }),
             "administrators": staffs.map({ $0.encodeJSON() }),
             "venues": venues.map({ $0.encodeJSON() }),
@@ -26,6 +32,9 @@ extension Conference: Decodable {
     public static func decode(_ e: Extractor) throws -> Conference {
         return try Conference(
             id: Id(value: e <| "id"),
+            title: e <| "title",
+            about: e <| "description",
+            contact: e <| "contact_information",
             schedules: e <|| "dates",
             staffs: e <|| "administrators",
             venues: e <|| "venues",
@@ -37,8 +46,8 @@ extension Conference: Decodable {
 
 extension Conference: CustomStringConvertible {
     public var description: String {
-        return "Conference(id: \(id), schedules: \(schedules), staffs: \(staffs), " +
-        "venues: \(venues), sponsors: \(sponsors), tracks: \(tracks))"
+        return "Conference(id: \(id), title: \(title), about: \(about), contact: \(contact), schedules: \(schedules), " +
+        "staffs: \(staffs), venues: \(venues), sponsors: \(sponsors), tracks: \(tracks))"
     }
 }
 
